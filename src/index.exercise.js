5fb232d974db0d6740c68346ce9dd98d5f800cc5
PouRia <pouria.bussiness@gmail.com>
2021-04-01 11:42:45 +0430

    feat: Added modal using Dialog component

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React , {useState} from 'react';
 import ReactDOM from 'react-dom'
 import {Logo} from './components/logo'
+import Dialog from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 const App = ()=>{
+    const [open, isOpen] = useState(false);
+    const [name, setName] = useState(null);
+
     function clickHandler(event){
         const {innerText : buttonName} = event.target;
-        alert(`You clicked ${buttonName}`)
+        setName(buttonName);
+        isOpen(true);
     }
+    
     return (
         <>
             <Logo width="80" height="80" />
             <h1>Bookshelf</h1>
-            <button onClick={clickHandler}>login</button><br/>
-            <button onClick={clickHandler}>register</button>
+            <button onClick={clickHandler}>Login</button><br/>
+            <button onClick={clickHandler}>Register</button>
+
+            <Dialog aria-label="Form" isOpen={open}>
+                <button onClick={() => isOpen(false)}>close</button>
+                <h1>{name}</h1>
+            </Dialog>
         </>
     )
 }
