f7d228c315d5c10c1f582ac60180e9e064fea111
PouRia <pouria.bussiness@gmail.com>
2021-04-02 20:21:04 +0430

    feat: used emotion custom styled

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -2,43 +2,35 @@ import styled from '@emotion/styled'
 import {Dialog as ReachDialog} from '@reach/dialog'
 
 // 🐨 create a button styled component here called "Button"
-// make it look nice and allow it to support a "variant" prop which can be
-// either "primary" or "secondary".
-// 💰 don't forget to export it at the bottom!
-// 💰 In my final version, I style padding, border, lineHeight, and borderRadius
-//    the same for both types, and then change the background and color based
-//    on the given variant.
-// 🦉 remember, you don't have to make things look perfect or just like they
-// do in the final example. Just make sure you understand how to create the
-// styled component and accept a prop to change which styles apply.
+
+export const Button = styled.button(props => {
+  return {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+    background: props.variant === 'primary' ? '#3f51b5' : '#f1f2f7',
+    color: props.variant === 'primary' ? 'white' : '#434449',
+
+  }
+})
+
 
 // 🐨 Feel free to create as many reusable styled components here as you'd like
 // 💰 in my finished version I have: Button, Input, CircleButton, Dialog, FormGroup
 
-// 🎨 here are a bunch of styles you can copy/paste if you want
-// Button:
-//   padding: '10px 15px',
-//   border: '0',
-//   lineHeight: '1',
-//   borderRadius: '3px',
+export const Input = styled.input`
 
-// Button variant="primary" (in addition to the above styles)
-//   background: '#3f51b5',
-//   color: 'white',
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+`
 
-// Button variant="secondary" (in addition to the above styles)
-//   background: '#f1f2f7',
-//   color: '#434449',
-
-// Input
-//   borderRadius: '3px',
-//   border: '1px solid #f1f1f4',
-//   background: '#f1f2f7',
-//   padding: '8px 12px',
-
-// FormGroup
-//   display: 'flex',
-//   flexDirection: 'column',
+export const FormGroup = styled.div`
+    display: 'flex',
+    flexDirection: 'column'
+`
 
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
