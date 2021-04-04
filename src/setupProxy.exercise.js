54b3c6b8b157f8a7ab9ffc46b565cbe1e66cd16a
PouRia <pouria.bussiness@gmail.com>
2021-04-04 20:18:08 +0430

    feat: Added redirect config

--- src/setupProxy.exercise.js
+++ src/setupProxy.exercise.js
@@ -1,4 +1,7 @@
 // 🐨 you don't need to do anything in this file for the exercise. This is
 // just here for the extra credit. See the instructions for more info.
 
-module.exports = () => {}
+function proxy(app){
+    app.get(/^\/$/, (request , response)=> response.redirect('/discover')  )
+}
+module.exports = proxy
