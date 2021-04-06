0368780532fdd413c48c386f36e23e84c37d8fc8
PouRia <pouria.bussiness@gmail.com>
2021-04-06 20:53:21 +0430

    refactor : created AuthProvider component

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
+import * as auth from 'auth-provider'
+
 const AuthContext = React.createContext();
 
 const useAuth = ()=>{
@@ -8,4 +13,59 @@ const useAuth = ()=>{
     }
     return contextValue
 }
-export {AuthContext, useAuth}
+
+async function getUser() {
+    let user = null
+  
+    const token = await auth.getToken()
+    if (token) {
+      const data = await client('me', {token})
+      user = data.user
+    }
+  
+    return user
+  }
+
+
+function AuthProvider(props) {
+    const {
+      data: user,
+      error,
+      isLoading,
+      isIdle,
+      isError,
+      isSuccess,
+      run,
+      setData,
+    } = useAsync()
+  
+    React.useEffect(() => {
+      run(getUser())
+    }, [run])
+  
+    const login = form => auth.login(form).then(user => setData(user))
+    const register = form => auth.register(form).then(user => setData(user))
+    const logout = () => {
+      auth.logout()
+      setData(null)
+    }
+  
+    if (isLoading || isIdle) {
+      return <FullPageSpinner />
+    }
+  
+    if (isError) {
+      return <FullPageErrorFallback error={error} />
+    }
+  
+    if (isSuccess) {
+      const value = {user, login, register, logout}
+        return (
+            <AuthContext.Provider value={value} {...props}/>
+        )
+      
+    }
+}
+
+
+export {AuthProvider,useAuth} 
