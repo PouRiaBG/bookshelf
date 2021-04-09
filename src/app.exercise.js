c6bd54c07fabdf737f2abe5db7dd19b07608f317
PouRia <pouria.bussiness@gmail.com>
2021-04-09 21:16:22 +0430

    feat: Added prefetch for loading

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,7 +3,9 @@ import {useAuth} from './context/auth-context'
 import {FullPageSpinner} from './components/lib'
 
 
-const AuthenticatedApp = React.lazy(()=> import('./authenticated-app'))
+const AuthenticatedApp = React.lazy(()=> 
+  import(/* webpackPrefetch: true */ './authenticated-app')
+)
 const UnauthenticatedApp = React.lazy(()=> import('./unauthenticated-app'))
 
 function App() {
